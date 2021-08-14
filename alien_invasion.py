ddb884c6e45c9e98e5e07a745474db3104cfad01
hawei <13597213421@163.com>
2021-08-14 15:31:31 +0800

    12.3.3 create a setting class. will manage all of setting on our game.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,21 +1,20 @@
 import sys
 
 import pygame
+from settings import Settings
 
 
 class AlienInvasion:
     def __init__(self):
         # 初始化 pygame对象
         pygame.init()
+        self.settings = Settings()
 
         # 设置屏幕大小
         # set_mode: 屏幕大小设置接口
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         # 设置游戏窗口标题
         pygame.display.set_caption("Alien Invasion")
-
-        # 设置游戏背景色
-        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         # 开启游戏循环
@@ -27,7 +26,7 @@ class AlienInvasion:
 
             # 每次循环重绘屏幕
             # fill: 颜色填充接口， 只接受一个颜色
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
 
             # 重新渲染屏幕（先擦出再渲染）可以表达位置的移动
             # 使屏幕可见
